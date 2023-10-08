9764e7b53b8de878634a5989103d298a06ac2d0d
Ksenia <418875603@mail.ru>
2023-10-08 22:16:43 +0300

    massive

---
+++ homeWork4/task 29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран. Вывод сделать отдельным методом.
+
+int [] array = new int [8];
+int size = 8;
+int a = array [0];
+array [0] = 0;
+Console.Write("[");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array [i] = new Random().Next(0, 11);
+    Console.Write(" " + array[i] + ",");
+}
+Console.Write("]");
