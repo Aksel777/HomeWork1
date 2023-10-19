1abe428a545d7b860e154e396e9cdf3a69a2956e
Ksenia <418875603@mail.ru>
2023-10-19 17:52:26 +0300

    yes\no

---
+++ с# сeминар 5/task 3/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] RandomArray(int length, int leftBorder, int rightBorder)
+{
+    int[] array = new int[length];
+
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i]= Random.Shared.Next(leftBorder, rightBorder+1);
+    }
+    return array;
+}
+
+bool SortArray(int[]array, int num)
+{
+    for(int i=0; i<array.Length; i++)
+    {
+        if(array[i] == num)
+        {
+            return true;
+        }
+        
+    }
+return false;
+}
+
+int[]myArray = RandomArray(10, -10, 10);
+Console.WriteLine(string.Join(", ", myArray));
+bool result = SortArray(myArray,7);
+Console.WriteLine(result);
