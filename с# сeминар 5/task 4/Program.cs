13f08aaa1efcea2cc7d08eb6445e065052ca0492
Ksenia <418875603@mail.ru>
2023-10-25 00:27:03 +0300

    dontknow

---
+++ с# сeминар 5/task 4/Program.cs
@@ -0,0 +1,30 @@
+﻿static int[] GenerateArray(int size)
+{
+    Random random = new Random();
+    int[] array = new int[size];
+
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i] = random.Next(1,10);
+    }
+    return array;
+}
+
+static int[] NewArray(int[] array)
+{
+    int[] newArray = new int[array.Length/2 + array.Length%2];
+    for(int i=0; i<array.Length; i++)
+    {
+        newArray[i] = array[i] * array[(array.Length-1) - i];
+        if(i == (array.Length-1) - i)
+        {
+            newArray[i] = array[i];
+        }
+    }
+    return newArray;
+}
+
+int[]array = GenerateArray(size:5);
+Console.Write(String.Join(" ", array));
+Console.WriteLine();
+Console.Write(NewArray(array));
