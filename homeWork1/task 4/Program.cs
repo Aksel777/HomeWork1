062bb7738e45b95ba06e8554164dab0c9e70e46a
Ksenia <418875603@mail.ru>
2023-10-25 00:17:28 +0300

    false

--- homeWork1/task 4/Program.cs
+++ homeWork1/task 4/Program.cs
@@ -1,21 +1,28 @@
-﻿Console.WriteLine("enter 1st number");
+﻿Console.Clear();
+
+Console.WriteLine("enter 1st number");
 int number1 = int.Parse(Console.ReadLine());
+
 Console.WriteLine("enter 2nd number");
 int number2 = int.Parse(Console.ReadLine());
+
 Console.WriteLine("enter 3rd number");
 int number3 = int.Parse(Console.ReadLine());
-int max = int.Parse(Console.ReadLine());
+
 
 if(number1 > number2 && number1 > number3)
 {
-    Console.WriteLine("max"+number1);
+    Console.WriteLine(" max " + number1);
 }
-if(number2 > number1 && number2 > number3)
+else if(number2 > number1 && number2 > number3)
 {
-    Console.WriteLine("max"+number2);
+    Console.WriteLine(" max " + number2);
 }
-if(number3 > number1 && number3 > number2)
+else if(number3 > number1 && number3 > number2)
 {
-    Console.WriteLine("max"+number3);
+    Console.WriteLine(" max " + number3);
 }
-
+else
+{
+    Console.Write("There is no one max");
+}
