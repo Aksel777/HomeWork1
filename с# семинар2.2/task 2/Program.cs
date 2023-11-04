e7aa306a35c959832d92ae57f327ac619eb47805
Ksenia <418875603@mail.ru>
2023-11-04 18:11:49 +0300

    seminar

---
+++ с# семинар2.2/task 2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+switch(number)
+{
+    case 1:
+    Console.WriteLine("x(>0) y(>0)");
+    break;
+    case 2:
+    Console.WriteLine("x(<0) y(>0)");
+    break;
+
+    case 3:
+    Console.WriteLine("x(<0) y(<0)");
+    break;
+
+    case 4:
+    Console.WriteLine("x(>0) y(<0)");
+    break;
+
+    default:
+    Console.WriteLine("uncorrected value");
+    break;
+}
