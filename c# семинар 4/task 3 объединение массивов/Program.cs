3b53a0d9e6452de901e2a31d814a86a0937de038
Ksenia <418875603@mail.ru>
2023-11-05 21:16:19 +0300

    massiv

---
+++ c# семинар 4/task 3 объединение массивов/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("enter size");
+int n = Convert.ToInt32(Console.ReadLine());
+Random rand = new Random();
+int[] arr1 = new int[n];
+int[] arr2 = new int[n];
+int[] arr3 = new int[n];
+
+for (int i=0; i<arr1.Length; i++)
+{
+    arr1[i] = rand.Next(0,50);
+    arr2[i] = rand.Next(0,50);
+    arr3[i] = arr1[i] + arr2[i];
+}
+
+for (int i=0; i<arr1.Length; i++)
+{
+    Console.Write(arr1[i] + " ");
+}
+Console.WriteLine();
+for (int i=0; i<arr1.Length; i++)
+{
+    Console.Write(arr2[i] + " ");
+}
+Console.WriteLine();
+for (int i=0; i<arr1.Length; i++)
+{
+    Console.Write(arr3[i] + " ");
+}
