fd4bb4b8fd3eb4932400bd8e558673521a27c928
Ksenia <418875603@mail.ru>
2023-11-10 02:37:45 +0300

    new tasks

---
+++ c# семинар 6/task 1/Program.cs
@@ -0,0 +1,29 @@
+﻿// развернуть массив
+
+int[] RandomArray(int length, int leftBorder, int rightBorder)
+{
+    int[] array = new int[length];
+
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i]= Random.Shared.Next(leftBorder, rightBorder+1);
+    }
+    return array;
+}
+ void ReverseArray (int[] sourceArray)
+ {
+    for(int i=0; i<sourceArray.Length/2; i++)
+    {
+        int temp = sourceArray[i];
+        sourceArray[i] = sourceArray[sourceArray.Length-i-1];
+        sourceArray[sourceArray.Length-i-1] = temp;
+    }
+ }
+
+int length = 7;
+int leftBorder = -10;
+int rightBorder = 10;
+ int[] myArray = RandomArray(length, leftBorder, rightBorder);
+ Console.WriteLine(string.Join(", ", myArray));
+ ReverseArray(myArray);
+ Console.WriteLine(string.Join(", ", myArray));
---
+++ c# семинар 6/task 2/Program.cs
@@ -0,0 +1,57 @@
+﻿// преобразовать исло из десятичной в двоичную систему
+
+
+// Console.WriteLine("enter number");
+// int number = int.Parse(Console.ReadLine());
+
+// int convert(int arg1)
+// {
+//     int size = 0;
+//     int number = arg1;
+//     // for(int i=0; arg1>0; i ++)
+//     while(arg1>0)
+//     {
+//         arg1 = arg1/2;
+//         size++;
+//     }
+// return size;
+// }
+
+// int[]FillArray(int size, int number)
+// {
+//     int [] array = new int[size];
+//     for (int i=0; i<array.Length; i++)
+//     {
+//         array[i] = number%2;
+//         number = number/2;
+//     }
+// return array;
+// }
+
+// int[] ReverseArray(int[]array)
+// {
+//     for (int i=0; i<array.Length/2; i++)
+//     {
+//         int temp = array[i];
+//         array[i] = array[array.Length-i-1];
+//         array[array.Length-i-1] = temp;
+//     }
+//     return array;
+// }
+
+
+// convert(number);
+// int[]array = FillArray(convert(number), number);
+
+// Console.WriteLine(string.Join(" ,", ReverseArray(array)));
+
+int number = int.Parse(Console.ReadLine());
+
+void  PrintBinaryView(int number)
+{
+if(number <= 0) return;
+PrintBinaryView(number/2);
+Console.Write(number%2);
+}
+PrintBinaryView(number);
+
